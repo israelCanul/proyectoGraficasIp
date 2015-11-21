ef1869712a024497732da86d4b7847e4d7926e4f
iran canul <iran canul>
2015-11-21 17:46:23 -0600

    se termina la vista y el pintedo de los datos con el plugin de graficos usados

--- controller/controllador.js
+++ controller/controllador.js
@@ -1,16 +1,21 @@
 var mongoose= require('mongoose');
 var Dato = mongoose.model('dato');
-
+var contDatos= require('./controllador');
 //GET - Funcion para regresar todos los books de la coleccion
 //GET - This function return all books on the colection
 exports.findAllDato = function(io) {
   //console.log("solicito");
    Dato.find(function(err, msj) {
       if(err) res.send(500, err.message);
-      ////console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
 	});	
 };
+
+
+
 
 exports.getDataAutoComplete = function(io) {
   //console.log("solicito");
@@ -20,7 +25,6 @@ exports.getDataAutoComplete = function(io) {
       ////console.log('GET /messages'+msj);
       if(msj.length>0){
         msj.map(function(mnsj){
-
           strDest.push ({
             label: mnsj.fecha,
             categoria:"Fecha"
@@ -62,7 +66,7 @@ exports.getDataAutoComplete = function(io) {
             categoria:"Puerto destino"
           });                                                                     
         });
-        console.log(strDest);
+        //console.log(strDest);
         io.emit('get data autoComplete',strDest);
       }
       
@@ -70,75 +74,264 @@ exports.getDataAutoComplete = function(io) {
   }); 
 };
 
+exports.separar=function(datos){
+  var valueColumn={'fecha':'0','hora':'1','tipo':'2','prioridad':'3','clasificacion':'4','protocolo':'5','ip_origen':'6','puerto_origen':'7','ip_destino':'8','puerto_destino':'9'};  
+  var general=[],Fecha={},Hora={},Tipo={},Prioridad={},Clasificacion={},Protocolo={},Ip_Origen={},Puerto_Origen={},Ip_Destino={},Puerto_Destino={};
+  var Fecha1=[],Hora1=[],Tipo1=[],Prioridad1=[],Clasificacion1=[],Protocolo1=[],Ip_Origen1=[],Puerto_Origen1=[],Ip_Destino1=[],Puerto_Destino1=[];
+
+  datos.map(function(item){  
+    // se arma el array para las fechas
+    if(typeof(Fecha["Fecha_"+item.fecha])=='undefined'){
+      Fecha1.push({'nombre':"Fecha_"+item.fecha});
+      Fecha["Fecha_"+item.fecha]={
+        'x':0,
+        'y':1
+      };
+    }else{
+      Fecha["Fecha_"+item.fecha].y++;
+    }
+    //se arma el array para las horas
+    if(typeof(Hora["Hora_"+item.hora])=='undefined'){
+      Hora1.push({'nombre':"Hora_"+item.hora});
+      Hora["Hora_"+item.hora]={
+        'x':1,
+        'y':1
+      };
+    }else{
+      Hora["Hora_"+item.hora].y++;
+    }
+    //se arma el array para el tipo
+    if(typeof(Tipo["Tipo_"+item.tipo])=='undefined'){
+      Tipo1.push({'nombre':"Tipo_"+item.tipo});
+      Tipo["Tipo_"+item.tipo]={
+        'x':2,
+        'y':1
+      };
+    }else{
+      Tipo["Tipo_"+item.tipo].y++;
+    }
+    //se arma el array para la prioridad
+    if(typeof(Prioridad["Prioridad_"+item.prioridad])=='undefined'){
+      Prioridad1.push({'nombre':"Prioridad_"+item.prioridad});
+      Prioridad["Prioridad_"+item.prioridad]={
+        'x':3,
+        'y':1
+      };
+    }else{
+      Prioridad["Prioridad_"+item.prioridad].y++;
+    }
+    //se arma el array para la Clasificacion
+    if(typeof(Clasificacion["Clasificacion_"+item.clasificacion])=='undefined'){
+      Clasificacion1.push({'nombre':"Clasificacion_"+item.clasificacion});
+      Clasificacion["Clasificacion_"+item.clasificacion]={
+        'x':4,
+        'y':1
+      };
+    }else{
+      Clasificacion["Clasificacion_"+item.clasificacion].y++;
+    }  
+    //se arma el array para el Protocolo
+    if(typeof(Protocolo["Protocolo_"+item.protocolo])=='undefined'){
+      Protocolo1.push({'nombre':"Protocolo_"+item.protocolo});
+      Protocolo["Protocolo_"+item.protocolo]={
+        'x':5,
+        'y':1
+      };
+    }else{
+      Protocolo["Protocolo_"+item.protocolo].y++;
+    }  
+    //se arma el array para la Ip de Origen
+    if(typeof(Ip_Origen["Ip_Origen_"+item.ip_origen])=='undefined'){
+      Ip_Origen1.push({'nombre':"Ip_Origen_"+item.ip_origen});
+      Ip_Origen["Ip_Origen_"+item.ip_origen]={
+        'x':6,
+        'y':1
+      };
+    }else{
+      Ip_Origen["Ip_Origen_"+item.ip_origen].y++;
+    } 
+    // se arma el array para los puertos origen    
+    if(typeof(Puerto_Origen["Puerto Origen_"+item.puerto_origen])=='undefined'){
+      Puerto_Origen1.push({'nombre':"Puerto Origen_"+item.puerto_origen});
+      Puerto_Origen["Puerto Origen_"+item.puerto_origen]={
+        'x':7,
+        'y':1
+      };
+      //console.log(item.puerto_origen);
+    }else{
+      Puerto_Origen["Puerto Origen_"+item.puerto_origen].y++;
+      //console.log(item.puerto_origen);
+    }     
+    //se arma el array para la Ip de destino
+    if(typeof(Ip_Destino["Ip_Destino_"+item.ip_destino])=='undefined'){
+      Ip_Destino1.push({'nombre':"Ip_Destino_"+item.ip_destino});
+      Ip_Destino["Ip_Destino_"+item.ip_destino]={
+        'x':8,
+        'y':1
+      };
+    }else{
+      Ip_Destino["Ip_Destino_"+item.ip_destino].y++;
+    }                           
+    //se arma el array para Puerto de Destino
+    if(typeof(Puerto_Destino["Puerto_Destino_"+item.puerto_destino])=='undefined'){
+      Puerto_Destino1.push({'nombre':"Puerto_Destino_"+item.puerto_destino});
+      Puerto_Destino["Puerto_Destino_"+item.puerto_destino]={
+        'x':9,
+        'y':1
+      };
+    }else{
+      Puerto_Destino["Puerto_Destino_"+item.puerto_destino].y++;
+    } 
+  });
+  Fecha1.map(function(date){
+    //console.log(Fecha[date.nombre]);
+    date.x=Fecha[date.nombre].x;
+    date.y=Fecha[date.nombre].y;    
+  });
+  Hora1.map(function(hora){
+    //console.log(Hora[hora.nombre]);
+    hora.x=Hora[hora.nombre].x;
+    hora.y=Hora[hora.nombre].y;    
+  });
+  Tipo1.map(function(tipo){
+    //console.log(Tipo[tipo.nombre]);
+    tipo.x=Tipo[tipo.nombre].x;
+    tipo.y=Tipo[tipo.nombre].y;    
+  });
+  Prioridad1.map(function(prioridad){
+    //console.log(Prioridad[prioridad.nombre]);
+    prioridad.x=Prioridad[prioridad.nombre].x;
+    prioridad.y=Prioridad[prioridad.nombre].y;    
+  });
+  Clasificacion1.map(function(clasificacion){
+    //console.log(Clasificacion[clasificacion.nombre]);
+    clasificacion.x=Clasificacion[clasificacion.nombre].x;
+    clasificacion.y=Clasificacion[clasificacion.nombre].y;    
+  });
+  Protocolo1.map(function(protocolo){
+    //console.log(Protocolo[protocolo.nombre]);
+    protocolo.x=Protocolo[protocolo.nombre].x;
+    protocolo.y=Protocolo[protocolo.nombre].y;    
+  });
+  Ip_Origen1.map(function(ip_origen){
+    //console.log(Ip_Origen[ip_origen.nombre]);
+    ip_origen.x=Ip_Origen[ip_origen.nombre].x;
+    ip_origen.y=Ip_Origen[ip_origen.nombre].y;    
+  });
+  Puerto_Origen1.map(function(puerto_origen){
+    //console.log(Puerto_Origen[puerto_origen.nombre]);
+    puerto_origen.x=Puerto_Origen[puerto_origen.nombre].x;
+    puerto_origen.y=Puerto_Origen[puerto_origen.nombre].y;    
+  });
+  Ip_Destino1.map(function(ip_destino){
+    //console.log(Ip_Destino[ip_destino.nombre]);
+    ip_destino.x=Ip_Destino[ip_destino.nombre].x;
+    ip_destino.y=Ip_Destino[ip_destino.nombre].y;    
+  });
+  Puerto_Destino1.map(function(puerto_destino){
+   //
+    puerto_destino.x=Puerto_Destino[puerto_destino.nombre].x;
+    puerto_destino.y=Puerto_Destino[puerto_destino.nombre].y;    
+  });  
+  general=[
+    Fecha1,Hora1,Tipo1,Clasificacion1,Prioridad1,Protocolo1,Ip_Origen1,Puerto_Origen1,Ip_Destino1,Puerto_Destino1
+  ];
+
+
+  console.log(general);
+  return general;
+}
 
 exports.findDatoByFecha = function(io,date) {
+     
    Dato.find({fecha:date},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
-      io.emit('get data', msj);    
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos); 
+      io.emit('get data', msj);
+
   }); 
 };
 
 exports.findDatoByTime = function(io,time) {
    Dato.find({hora:time},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
 exports.findDatoByPriority = function(io,priority) {
    Dato.find({prioridad:priority},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
 exports.findDatoByType = function(io,type) {
    Dato.find({tipo:type},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
 exports.findDatoByClassification = function(io,classification) {
    Dato.find({clasificacion:classification},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
 exports.findDatoByProtocol = function(io,protocol) {
    Dato.find({protocolo:protocol},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
 exports.findDatoByIpOrigin = function(io,IpOrigin) {
    Dato.find({ip_origen:IpOrigin},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
 exports.findDatoByPortOrigin = function(io,portOrigin) {
    Dato.find({puerto_origen:portOrigin},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
 exports.findDatoByIpDestination = function(io,IpDestination) {
    Dato.find({ip_destino:IpDestination},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
 exports.findDatoByPortDestination = function(io,portDestination) {
    Dato.find({puerto_destino:portDestination},function(err, msj) {
       if(err) res.send(500, err.message);
-      //console.log('GET /messages'+msj);
+      var graficos=contDatos.separar(msj);
+      //console.log(graficos);
+      io.emit('graphics', graficos);
       io.emit('get data', msj);    
   }); 
 };
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	var socket=io();
 	var datosAutocomplete=null;
+	var point=[];
+	var arrayPuntos=[];
 	//llamamos y cargamos los datos para el autocomplete
 	socket.emit('get data autoComplete');
 	//habilitamos la funcion para cargar los datos ya sea de la busqueda en general o de la que realicemos con el 
@@ -9,31 +11,202 @@ $(document).ready(function(){
 		$('#list-msgs').html("");
 	var datos=data;
 		if(datos.length>0){
-			console.log("son mas de 0 : "+ datos.length);
+			//console.log("son mas de 0 : "+ datos.length);
 			
 			//console.log(datos);
+			$('#list-msgs').html('<tr><th>Fecha</th><th>Hora</th><th>Tipo</th><th>Clasificacion</th><th>Prioridad</th><th>Protocolo</th><th>Ip Origen</th><th>Puerto Origen</th><th>Ip Destino</th><th>Puerto Destino</th><tr>');
 			datos.map(function(dato){
-				$('#list-msgs').append('<div>'+  dato.fecha+", "+
-	  dato.hora +", "+
-	  dato.tipo+", "+
-	  dato.prioridad+", "+
-	  dato.clasificacion+", "+
-	  dato.protocolo+", "+
-	  dato.ip_origen+", "+
-	  dato.puerto_origen+", "+
-	  dato.ip_destino+", "+
-	  dato.puerto_destino+'</div>');			
+
+
+				$('#list-msgs').append(
+									'<tr><td>'+  dato.fecha+"</td><td>"+
+									dato.hora +"</td><td>"+
+									dato.tipo+"</td><td>"+
+									dato.prioridad+"</td><td>"+
+									dato.clasificacion+"</td><td>"+
+									dato.protocolo+"</td><td>"+
+									dato.ip_origen+"</td><td>"+
+									dato.puerto_origen+"</td><td>"+
+									dato.ip_destino+"</td><td>"+
+									dato.puerto_destino+'</td></tr>');
+									  			
 			});
 		}
       
     });
+    // socket para los graficos
+	socket.on('graphics', function (data) {
+	//console.log(data[0]);	
+	arrayPuntos=[];
+	var datos=data;
+/*	console.log(datos.length);
+	console.log(datos[0]);
+	console.log(datos[2]);*/
+	for (var i = 0 ; i < datos.length; i++) {
+		console.log(datos[i]);
+		point=[];
+		datos[i].map(function(dato){
+			tempPoint=[];
+			tempPoint.push(dato.x,dato.y);
+			point.push(tempPoint);
+		});
+		arrayPuntos.push(point);
+	};
+	//console.log(datos.length);
+	/*datos.map(function(item){
+
+		//console.log("son mas de 0 : "+ datos.length);
+		tempPoint=[];
+		item.map(function(dato){
+			tempPoint=[];
+			tempPoint.push(dato.x,dato.y);
+			point.push(tempPoint);
+		});
+		arrayPuntos.push(point);
+		console.log(arrayPuntos);
+
+	});*/
+	
+
+			
+	      	/*pointFecha.push(tempPoint);*/
+	      	//console.log(pointFecha);
+ $('#container').highcharts({
+        chart: {
+            type: 'scatter',
+            zoomType: 'xy'
+        },
+        data: {
+            table: 'columns'
+        },
+        title: {
+            text: 'Grafica de dispersion de los datos Filtrados'
+        },
+        subtitle: {
+            text: 'Mediante Consulta - Tabla Anterior'
+        },
+        xAxis: {
+            title: {
+                enabled: true,
+                text: 'Valores de La tabla'
+            },
+            categories: ['Fecha', 'Hora', 'Tipo','Clasificacion','Prioridad','Protocolo','Ip origen','Puerto origen','Ip destino','Puerto destino'],
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true
+        },
+        yAxis: {
+            title: {
+                text: 'Contadores'
+            }
+        },
+/*        legend: {
+            layout: 'horizontal',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 100,
+            y: 70,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+            borderWidth: 1
+        },*/
+        plotOptions: {
+            scatter: {
+                marker: {
+                    radius: 5,
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: 'rgb(100,100,100)'
+                        }
+                    }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+/*                tooltip: {
+                	footerFormat:'<b>{series.color}</b>',
+                    headerFormat: '<b>{series.name}</b><br>',
+                    pointFormat: '{point.x} cm, {point.y} kg<br>'
+                }*/
+            }
+        },
+        series: [
+        {
+        	name: 'Fecha',
+            color: 'rgba(223, 83, 83, .5)',
+            label:'Fechass',
+            data: arrayPuntos[0],
+
+        }, 
+        {
+        	name: 'Hora',
+            color: 'rgba(223, 83, 83, .5)',
+            data: arrayPuntos[1],
+
+        }, 
+        {
+        	name: 'Tipo',
+            color: 'rgba(223, 83, 83, .5)',
+            data: arrayPuntos[2],
+
+        }, 
+        {
+        	name: 'Clasificacion',
+            color: 'rgba(223, 83, 83, .5)',
+            data: arrayPuntos[3],
+
+        }, 
+        {
+        	name: 'Prioridad',
+            color: 'rgba(223, 83, 83, .5)',
+            data: arrayPuntos[4],
+
+        }, 
+        {
+        	name: 'Protocolo',
+            color: 'rgba(223, 83, 83, .5)',
+            data: arrayPuntos[5],
+
+        },
+        {
+        	name: 'Ip Origen',
+            color: 'rgba(223, 83, 83, .5)',
+            data: arrayPuntos[6]
+        }, 
+        {
+        	name: 'Puerto Origen',
+            color: 'rgba(223, 83, 83, .5)',
+            data: arrayPuntos[7],
+
+        }, 
+        {
+        	name: 'Ip Destino',
+            color: 'rgba(223, 83, 83, .5)',
+            data: arrayPuntos[8],
+
+        },                                                 
+        {
+            name: 'Puerto Destino',
+            color: 'rgba(119, 152, 191, .5)',
+            data: arrayPuntos[9],
+        }]
+    });
+
+
+
+    });    
 	//habilitamos la funcion para que cada vez que se envien los datos de busqueda se carge y reinicialize el autocomplete
 	socket.on('get data autoComplete', function (datos) {
-		console.log(datos);
+		//console.log(datos);
 		datosAutocomplete=datos; 
 	    $("#busqueda").MixCombo({
 	        	    delay: 0,
-				    minLength: 3,
+				    minLength: 1,
 				    source: function(e, t) {
 				        var o = new RegExp($.ui.autocomplete.escapeRegex(e.term), "i");
 				        t($.grep(datos, function(e) {
@@ -44,7 +217,7 @@ $(document).ready(function(){
 				        $("#busqueda").val("");
 				    },
 				    select: function(e, t) {
-				        console.log(t.item.label);
+				        //console.log(t.item.label);
 				        // aqui se cargan las funciones correspondientes de acuerdo al tipo de busqueda que realizen
 				        if(t.item.categoria=='Fecha'){
 				        	socket.emit('get data by date',t.item.label);
